26c62221cff99190779afbec537ca6dc0cd76b17
nales <>
2022-08-21 09:28:00 +0000

    Put the CLI version process into its own class
    FossilOrigin-Name: 95b45519b03c1f637752866c4b464cf6efa64c6d75510cbc732dd760c6d482cb

--- src/Main.hx
+++ src/Main.hx
@@ -1,55 +1,9 @@
 package;
 
-typedef CommandArguments = {
-    var filepath:String;
-}
-
 class Main
 {
     public static function main()
     {
-        var arguments = parseCommandArguments();
-        var filepath = arguments.filepath;
-        if(!sys.FileSystem.exists(filepath))
-        {
-            printError('"$filepath" does not exist.');
-            return;
-        }
-        if(sys.FileSystem.isDirectory(filepath))
-        {
-            printError('"$filepath" is a directory.');
-            return;
-        }
-        var content = sys.io.File.getContent(filepath);
-
-        var parser = new Parser();
-        parser.parse(content);
-        if(!parser.hasError)
-        {
-            Sys.println(parser.getResultAsString());
-        }
-        else
-        {
-            printError('${parser.errorMessage}');
-        }
-    }
-
-    public static function printError(message:String):Void
-    {
-        Sys.stderr().writeString('$message\n');
-        Sys.stderr().flush();
-    }
-
-    private static function parseCommandArguments():CommandArguments
-    {
-        var arguments = {
-            filepath : ""
-        };
-        var args = Sys.args();
-        if(args.length >= 1)
-        {
-            arguments.filepath = args[0];
-        }
-        return arguments;
+        new VersionCli();
     }
 }
---
+++ src/VersionCli.hx
@@ -0,0 +1,58 @@
+package;
+
+typedef CommandArguments = {
+    var filepath:String;
+}
+
+/**
+ * The CLI version.
+ */
+class VersionCli
+{
+    public function new()
+    {
+        var arguments = parseCommandArguments();
+        var filepath = arguments.filepath;
+        if(!sys.FileSystem.exists(filepath))
+        {
+            printError('"$filepath" does not exist.');
+            return;
+        }
+        if(sys.FileSystem.isDirectory(filepath))
+        {
+            printError('"$filepath" is a directory.');
+            return;
+        }
+        var content = sys.io.File.getContent(filepath);
+
+        var parser = new Parser();
+        parser.parse(content);
+        if(!parser.hasError)
+        {
+            Sys.println(parser.getResultAsString());
+        }
+        else
+        {
+            printError('${parser.errorMessage}');
+        }
+    }
+
+    public static function printError(message:String):Void
+    {
+        Sys.stderr().writeString('$message\n');
+        Sys.stderr().flush();
+    }
+
+    private static function parseCommandArguments():CommandArguments
+    {
+        var arguments = {
+            filepath : ""
+        };
+        var args = Sys.args();
+        if(args.length >= 1)
+        {
+            arguments.filepath = args[0];
+        }
+        return arguments;
+    }
+}
